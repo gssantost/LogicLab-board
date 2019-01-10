3e62258bbca78234b2505d2207a1d60574b18099
gssantost <31294447+gssantost@users.noreply.github.com>
2019-01-10 02:44:27 -0400

    fixed response generation, cleaned

--- Tester/Tester.ino
+++ Tester/Tester.ino
@@ -6,114 +6,88 @@
 
 SoftwareSerial BluetoothSerial(BT_SERIAL_TX, BT_SERIAL_RX);
 
-                // { O, I, O, I, O, I, G, I, O, I,  O,  I,  O,  V } // Valor de Vcc queda por fuera (última posición). El arreglo tendría noPines - 1 (casos 14 y 16 pines). 
-int pinArray[13] = { 2, 3, 4, 5, 6, 7, 8, 8, 9, 10, 11, 12, 13 }; 
+int pinArray[13] = { 2, 3, 4, 5, 6, 7, 8, 8, 9, 10, 11, 12, 13 };
 
 String data;
 String response;
 int counter;
+int maxValues;
+int currentInput;
 
-const int capacity = 6*JSON_ARRAY_SIZE(2) + JSON_ARRAY_SIZE(6) + JSON_OBJECT_SIZE(3);
-const int responseCapacity = JSON_OBJECT_SIZE(3);
-
-/*
-* STATUS CODE
-* 1 = OK || SUCCESS
-* 2 = FAILURE || CONNECTION ERROR
-*/
+const int capacity = 6 * JSON_ARRAY_SIZE(2) + JSON_ARRAY_SIZE(6) + JSON_OBJECT_SIZE(3);
 
 void setup() {
-  
+
   Serial.begin(9600);
   Serial.println("Arduino is listening");
   BluetoothSerial.begin(9600);
-  
+
 }
 
 void loop() {
 
   if (BluetoothSerial.available()) {
-      String data = BluetoothSerial.readStringUntil("\n");
-      Serial.println(data);
-      DynamicJsonBuffer jsonBuffer(capacity);
-      JsonObject& obj = jsonBuffer.parseObject(data);
-   
-      if (obj.success()) {
-        response = "{\"status\":1,\"lecture\": [ ";             // abro el array de Lectura más externo => [ [0, 0, 0, 0], [0, 0, 0, 0] ]
-        //responseObj["status"] = 1;
-        obj.printTo(Serial); Serial.println("JSON parsed!");
-        
-        const int pinNo = obj["pinNo"];                        // Número de pines del chip
-        const char* pinConfig = obj["config"];                 // Cadena de configuración de entradas, salidas, Ground, VCC
-        
-        if (pinNo == 14) {
-          int testSize = obj["test"].size();
+    String data = BluetoothSerial.readStringUntil("\n");
+    Serial.println(data);
+    DynamicJsonBuffer jsonBuffer(capacity);
+    JsonObject& obj = jsonBuffer.parseObject(data);
 
-          // Recorre los valores de prueba
-          for (int i = 0; i < testSize; i++) {
-            int testCase = obj["test"][i].size(); Serial.println("Size de valores para el caso de prueba: " + testCase);
+    if (obj.success()) {
+      response = "{\"status\":1,\"result\": [";
+      obj.printTo(Serial);
 
-            int values[testCase];
+      const int pinNo = obj["pinNo"];
+      const char* pinConfig = obj["config"];
 
-            for (int j = 0; j < testCase; j++) {
-              values[j] = obj["test"][i][j].as<int>();
-            }
+      if (pinNo == 14) {
+        int testSize = obj["test"].size();
 
-            response += "[ ";                                 // abro el array de Lectura más interno, guardará el resultado de las N SALIDAS en respuesta al array de valores de prueba inicial            
-            executeTest(pinNo, pinConfig, pinArray, values, testCase);
-            response += "], ";                                 // cierro el array de Lectura más interno
-            
-            
-            /*if (testCase > 1) {
-              int values[testCase];
+        for (int i = 0; i < testSize; i++) {
+          int testCase = obj["test"][i].size();
+          int values[testCase];
 
-              for (int j = 0; j < testCase; j++) {
-                values[j] = obj["test"][i][j].as<int>();
-              }
-              
-              executeTest(pinNo, pinConfig, pinArray, values, testCase);
-            } else {
-              // do it simple
-              for (int j = 0; j < testCase; j++) {
-                auto val = obj["test"][i][j].as<int>(); Serial.println(val);   // Valor de prueba
-                simpleTest(pinNo, pinConfig, pinArray, getLowOrHigh(val));
-              }
-            }*/
+          for (int j = 0; j < testCase; j++) {
+            values[j] = obj["test"][i][j].as<int>();
           }
 
-          delay(1000);
-          setBaseState(pinArray);
-          
-          response += "],\"message\":\"Prueba realizada con éxito.\"}";          // cierro el array de Lectura más externo y concateno mensaje
+          response += "[";
+          executeTest(pinNo, pinConfig, pinArray, values, testCase);
+          if (i < testSize - 1) {
+            response += "], ";
+          } else {
+            response += "]";
+          }
+        }
 
-          emit(response);
-          
-        } // in case pinNo equals 14
-               
-      } else {
-        response = "{\"status\":2,\"message\":\"Error en JSON.\"}";
-        emit(response);                                                       // Envía la respuesta del intento
+        delay(1000);
+        setBaseState(pinArray);
+        response += "],\"message\":\"Prueba realizada con éxito.\"}";
+        emit(response);
       }
-      // fin de obj.success()
-      
+    } else {
+      response = "{\"status\":2,\"message\":\"Error en JSON.\"}";
+      emit(response);
+    }
   }
-  // fin de BluetoothSerial.available()
 }
 
 /**
- * Función que ejecuta una prueba
- */
+   Función que ejecuta una prueba
+*/
 void executeTest(int pinNo, char* pinConfig, int pinArray[], int testValues[], int valuesSize) {
-  
+
   counter = 0;
-  
+  currentInput = 0;
+  maxValues = 0;
+
   for (int i = 0; i < pinNo; i++) {
     if (pinConfig[i] == 'I') {
       pinMode(pinArray[i], INPUT);
       Serial.print("Pin ["); Serial.print(pinArray[i]); Serial.println("] configurado como ENTRADA.");
-    
-    } else if (pinConfig[i] == 'O') {
+      maxValues++;
       
+    } else if (pinConfig[i] == 'O') {
+
       pinMode(pinArray[i], OUTPUT);
 
       if (counter < valuesSize) {
@@ -133,19 +107,20 @@ void executeTest(int pinNo, char* pinConfig, int pinArray[], int testValues[], i
 
   for (int i = 0; i < pinNo; i++) {
     if (pinConfig[i] == 'I') {
-      Serial.print("Valor leído en Pin ["); Serial.print(pinArray[i]); Serial.print("] es: "); 
+      currentInput++;
+      Serial.print("Valor leído en Pin ["); Serial.print(pinArray[i]); Serial.print("] es: ");
       Serial.println(digitalRead(pinArray[i]));
       response += digitalRead(pinArray[i]);
-      response += ",";
+      if (currentInput < maxValues) {
+        response += ",";
+      }
       // Serial.println("Así vá la respuesta: " + response); // testing
     }
   }
-
-  //return response;
 }
 
 /**
- * Función que ejecuta una prueba de tipo simple
+   Función que ejecuta una prueba de tipo simple
 **/
 void simpleTest(int pinNo, char* pinConfig, int pinArray[], uint8_t testValue) {
   for (int i = 0; i < pinNo; i++) {
@@ -162,7 +137,7 @@ void simpleTest(int pinNo, char* pinConfig, int pinArray[], uint8_t testValue) {
 
   for (int i = 0; i < pinNo; i++) {
     if (pinConfig[i] == 'I') {
-      Serial.print("Valor leído en Pin ["); Serial.print(pinArray[i]); Serial.print("] es: "); 
+      Serial.print("Valor leído en Pin ["); Serial.print(pinArray[i]); Serial.print("] es: ");
       //delay(1000);
       Serial.println(digitalRead(pinArray[i]));
     }
@@ -170,7 +145,7 @@ void simpleTest(int pinNo, char* pinConfig, int pinArray[], uint8_t testValue) {
 }
 
 /*
-* Función que retorna un entero sin signo de 1 byte, LOW o HIGH
+  Función que retorna un entero sin signo de 1 byte, LOW o HIGH
 */
 uint8_t getLowOrHigh(int value) {
   if (value == 1) {
@@ -181,8 +156,8 @@ uint8_t getLowOrHigh(int value) {
 }
 
 /*
- * Función para setear el estado de las entradas/salidas a default una vez terminado el batch de pruebas
- */
+   Función para setear el estado de las entradas/salidas a default una vez terminado el batch de pruebas
+*/
 void setBaseState(int pinArray[]) {
   for (int i = 0; i < sizeof(pinArray); i++) {
     pinMode(pinArray[i], INPUT);
@@ -190,15 +165,15 @@ void setBaseState(int pinArray[]) {
 }
 
 /*
- * Función para enviar un mensaje a través del Bluetooth
- */
+   Función para enviar un mensaje a través del Bluetooth
+*/
 void emit(String message) {
-   Serial.println(message);
-   BluetoothSerial.print(message + "\n");
+  Serial.println(message);
+  BluetoothSerial.print(message + "\n");
 }
 
 /*
-* Función para limpiar la variable global de respuesta al finalizar el ciclo
+  Función para limpiar la variable global de respuesta al finalizar el ciclo
 */
 void clearMessage(String response) {
   response = "";
